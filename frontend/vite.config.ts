dfca26de32525f4d616801c92af50321c61732ef
Bruno Lopes <brunoomiguel10@hotmail.com>
2024-11-17 22:31:44 +0000

    Docker fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], // Ensure .tsx is included
+  },
+  base: "/",
+  preview: {
+    port: 5174,
+    strictPort: true,
+  },
+  server: {
+    port: 5174,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5174",
+  },
+  optimizeDeps: {
+    dedupe: ['react', 'react-dom'],
+  },
+});
